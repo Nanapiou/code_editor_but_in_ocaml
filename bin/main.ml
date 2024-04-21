27b77246cfb0036658b6126f42b530162c7f97da
Nanapiou <nana.juniot@gmail.com>
2024-04-21 14:20:15 +0200

    Refactor code to improve readability and maintainability

--- bin/main.ml
+++ bin/main.ml
@@ -1,85 +1,89 @@
-module Context = Map.Make (String)
+module Context = Map.Make(String)
 
-type expr =
+type expr = 
   | Int of int
   | Variable of string
-  | Abstraction of { param : string; body : expr }
-  | Application of { funct : expr; argument : expr }
+  | Application of { funct: expr; argument: expr}
+  | Abstraction of { param: string; body: expr }
 
 type value =
-  | VClosure of { context : value Context.t; param : string; body : expr }
-  | VNative of (value -> value)
   | VInt of int
+  | VClosure of { context: value Context.t; param: string; body: expr }
+  | VNative of (value -> value)
 
-exception Type_error
+exception Type_error of string
 
-let rec interp context expr =
-  match expr with
-  | Int v -> VInt v
-  | Variable name -> Context.find name context
-  | Abstraction { param; body } -> VClosure { context; param; body }
-  | Application { funct; argument } -> begin
-      let argument = interp context argument in
-      match interp context funct with
-      | VClosure { context; param; body } ->
-          interp (Context.add param argument context) body
-      | VNative f -> f argument
-      | VInt _ -> raise Type_error
-    end
+let rec interp context e =
+  match e with
+  | Int n -> VInt n
+  | Variable v -> Context.find v context
+  | Abstraction {param; body} -> VClosure {context; param; body}
+  | Application {funct; argument} -> 
+    let argument = interp context argument in 
+    match interp context funct with
+    | VNative f -> f argument
+    | VClosure {context; param; body} -> interp (Context.add param argument context) body
+    | _ -> raise (Type_error "Not a function")
 
-let _ =
-  Abstraction
-    {
-      param = "f";
-      body =
-        Application
-          {
-            funct = Variable "f";
-            argument =
-              Application
-                {
-                  funct = Variable "print_hello_world";
-                  argument = Variable "f";
-                };
-          };
-    }
+let initial_context =
+  Context.empty |>
+  Context.add "hello_world" (
+    VNative (fun v -> print_endline "Hello world!"; v)
+  ) |>
+  Context.add "add" (
+    VNative (fun a ->
+      VNative (fun b ->
+        match a, b with
+        | VInt a, VInt b -> VInt (a + b)
+        | _ -> raise (Type_error "Can only add two integers")
+      )
+    )
+  ) |>
+  Context.add "print_int" (
+    VNative (fun v ->
+      match v with
+      | VInt n -> Printf.printf "%d\n" n; v
+      | _ -> raise (Type_error "Can print an integer")
+    )
+  )
 
-(* ((λf.(f (print_hello_world f))) (λf.(f (print_hello_world f)))) *)
-let code = Application {
+let rec to_string = function
+  | Int n -> string_of_int n
+  | Variable v -> v
+  | Abstraction { param; body } -> String.concat "" [
+      "("; "λ"; param; ".";  to_string body; ")"
+    ]
+  | Application { funct; argument } -> String.concat "" [
+      "("; to_string funct; " "; to_string argument; ")"
+    ]
+
+let prog = Application {
   funct = Variable "print_int";
   argument = Application {
     funct = Application {
       funct = Variable "add";
-      argument = Int 5;
+      argument = Int 9;
     };
-    argument = Int 3;
-  };
+    argument = Int 5
+  }
 }
 
+let _ = interp initial_context prog
 
-let initial_context =
-  Context.empty
-  |> Context.add "print_hello_world"
-       (VNative
-          (fun v ->
-            print_endline "hello world";
-            v))
-  |> Context.add "add" 
-  (VNative 
-  (fun a -> 
-    VNative
-    (fun b -> 
-      match a, b with
-      | VInt a, VInt b -> VInt (a + b)
-      | _ -> raise Type_error
-    )
-  ))
-  |> Context.add "print_int" 
-  (VNative
-  (fun v -> 
-    match v with
-    | VInt w -> print_int w; print_newline (); v
-    | _ -> raise Type_error
-  ))
+let f = Abstraction {
+  param = "f";
+  body = Application {
+    funct = Variable "f";
+    argument = Application {
+      funct = Variable "hello_world";
+      argument = Variable "f"
+    }
+  }
+}
 
-let _x = interp initial_context code
+let infinite_prog = Application {
+  funct = f;
+  argument = f
+}
+
+let () = infinite_prog |> to_string |> print_endline
